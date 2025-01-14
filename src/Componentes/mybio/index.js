3db4f4d8df0cc6df27975401edd79f70387f3c4f
Bernardo Pereira <bernardo0kneezmz@gmail.com>
2025-01-14 09:49:42 -0300

    Ajustando mobile

--- src/Componentes/mybio/index.js
+++ src/Componentes/mybio/index.js
@@ -11,15 +11,15 @@ function Bio() {
         
             <h1>Bernardo Pereira    </h1>
             <p>
-            Olá! Sou Bernardo Pereira, tenho 21 anos e sou apaixonado por tecnologia e desenvolvimento de software. <br></br>Atualmente, trabalho como Analista de Sistemas Júnior, onde coloco em prática minhas habilidades em desenvolvimento full stack.
+            Olá! Sou Bernardo Pereira, tenho 21 anos e sou apaixonado por tecnologia e desenvolvimento de software. Atualmente, trabalho como Analista de Sistemas Júnior na Mais Distribuidora, onde coloco em prática minhas habilidades em desenvolvimento full stack.
 <br></br><br></br>
-Tenho experiência profissional desenvolvendo soluções completas, desde o front-end até o back-end, o que me permite entender e atuar em todas as etapas do ciclo de desenvolvimento de sistemas. Minha principal ferramenta de trabalho é o C#, mas estou sempre buscando expandir meus conhecimentos e explorar novas tecnologias.
+Tenho experiência profissional desenvolvendo soluções completas, desde o front-end até o back-end, o que me permite entender e atuar em todas as etapas do ciclo de desenvolvimento de sistemas. Minha principal ferramenta de trabalho é o C#, mas também possuo experiência com React e Node.js, tecnologias que uso para criar aplicações modernas e eficientes.
+<br></br><br></br>
+Diariamente, procuro desenvolver soluções inovadoras, sejam sites ou aplicações console, que contribuam para otimizar o funcionamento e o faturamento da empresa.
 <br></br><br></br>
 Meu objetivo é crescer como Desenvolvedor Full Stack, contribuindo para projetos que façam a diferença e superem expectativas.
 <br></br><br></br>
-Este site foi desenvolvido utilizando React, Node.js e outras tecnologias, proporcionando uma experiência enriquecedora de aprendizado e um grande avanço no meu desenvolvimento técnico.
-<br></br><br></br>
-Adoro aprender, compartilhar conhecimentos e colaborar em desafios
+Adoro aprender, compartilhar conhecimentos e colaborar em desafios.
             </p>
         </div>
 
